385f87b60923a302a83ee3a8e299612501258be0
Jonny Kuang <lightscreen.app@gmail.com>
2017-12-02 18:24:10 +0800

    Done

--- HackMenuController/ViewController.swift
+++ HackMenuController/ViewController.swift
@@ -12,14 +12,54 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        UIMenuController.shared.menuItems = (1 ... 10).map {
+            UIMenuItem.init(title: "Button \($0)", action: #selector(tapMenuItem))
+        }
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleViewDidMoveToSuperviewNotification), name: .viewDidMoveToSuperview, object: nil)
+        
+        struct Swizzle {
+            static let perform: Void = {
+                method_exchangeImplementations(
+                    class_getInstanceMethod(UIView.self, #selector(UIView.didMoveToSuperview))!,
+                    class_getInstanceMethod(UIView.self, #selector(UIView.swizzle_didMoveToSuperview))!
+                )
+            }()
+        }
+        _ = Swizzle.perform
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+private extension ViewController {
+    
+    static let calloutBarButtonClass = NSClassFromString("UICalloutBarButton") as? UIButton.Type
+    
+    @objc func handleViewDidMoveToSuperviewNotification(_ notification: Notification) {
+        guard let calloutBarButtonClass = ViewController.calloutBarButtonClass,
+            let button = notification.object as? UIButton,
+            button.isMember(of: calloutBarButtonClass),
+            !button.allTargets.contains(self) else { return }
+        
+        button.addTarget(self, action: #selector(tapMenuItemButton), for: .touchUpInside)
+    }
+    
+    @objc func tapMenuItemButton(_ button: UIButton) {
+        print(#function, button.currentTitle ?? "N/A")
+    }
+    
+    @objc func tapMenuItem() {}
+}
+
+
+
+@objc private extension UIView {
+    func swizzle_didMoveToSuperview() {
+        NotificationCenter.default.post(name: .viewDidMoveToSuperview, object: self)
+        swizzle_didMoveToSuperview()
+    }
+}
+
+private extension Notification.Name {
+    static let viewDidMoveToSuperview = Notification.Name("ViewDidMoveToSuperviewNotification")
+}
